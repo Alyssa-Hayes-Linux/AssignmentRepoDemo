5ce3facefa5d3e720855f2ce0e650504befa1649
EMunnik657 <carson657@gmail.com>
2024-10-28 19:32:43 -0400

    baseline version 2

--- Sprint 1/src/Profile.java
+++ Sprint 1/src/Profile.java
@@ -19,4 +19,75 @@ public class Profile {
     String password, boolean notification ){
 
     }
+
+    //getters and setters
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPronoun() {
+        return pronoun;
+    }
+
+    public void setPronoun(String pronoun) {
+        this.pronoun = pronoun;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isNotification() {
+        return notification;
+    }
+
+    public void setNotification(boolean notification) {
+        this.notification = notification;
+    }
+
+    //masks
+    /**
+     * needed for:
+     *  DOB
+     *  email
+     *  phone
+     *  password
+     */
+
+    /**
+     * toString for output
+     */
 }
