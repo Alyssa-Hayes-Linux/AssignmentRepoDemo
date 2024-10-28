b07e8cc764cd87efa709695569909c6f61635924
EMunnik657 <carson657@gmail.com>
2024-10-28 19:16:04 -0400

    baseline version 1

---
+++ Sprint 1/src/Profile.java
@@ -0,0 +1,22 @@
+public class Profile {
+
+    //base variables
+    private String name;
+    private String DOB;
+    private String email;
+    private String phone;
+    private String pronoun;
+    private String password;
+    private boolean notification;
+
+    //base obj
+    public Profile{
+
+    }
+
+    //obj to be initialized
+    public Profile(String name, String DOB, String email, String phone, String pronoun,
+    String password, boolean notification ){
+
+    }
+}
