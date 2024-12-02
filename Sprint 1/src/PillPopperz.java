877da2d0c19e15f2dab6eb06de4f64c61d193f07
Ben <herbstb@vcu.edu>
2024-12-02 02:32:13 -0500

    fixed some errors + aesthetics

--- Sprint 1/src/PillPopperz.java
+++ Sprint 1/src/PillPopperz.java
@@ -48,9 +48,9 @@ public class PillPopperz {
     public static int mainMenu() {
         Scanner input = new Scanner(System.in);
         System.out.println("Main Menu:");
-        System.out.println("1. Select or Create Profile");
-        System.out.println("2. View All Profiles");
-        System.out.println("3. Exit");
+        System.out.println("\t1. Select or Create Profile");
+        System.out.println("\t2. View All Profiles");
+        System.out.println("\t3. Exit");
         System.out.print("Please select an option:\n");
         return input.nextInt();
     }
@@ -77,6 +77,7 @@ public class PillPopperz {
      * @param profileList list of profiles
      * @return the index of the selected profile or option to create a new profile
      */
+
     public static int menuProfileSelect(ArrayList<Profile> profileList) {
         int profileNumber = 1;
         System.out.println("Please select a profile option:");
@@ -339,7 +340,7 @@ public class PillPopperz {
             }
 
             if (selected == profile.medList.size() + 1) {
-                profile.medList.add(newMed(profile));
+                newMed(profile);
                 flag = false;
             } else if (selected <= profile.medList.size()) {
                 System.out.println(profile.medList.get(selected - 1).medDetails());
@@ -373,7 +374,7 @@ public class PillPopperz {
 
         Med newMed = new Med();
         if(tempName.equals("test")){
-           return new Med("test");
+           profile.medList.add(new Med("test"));
         }
         // Input and validate medication name
         boolean flag = true;
@@ -414,32 +415,42 @@ public class PillPopperz {
         flag = true;
         // Input and validate medication days
         ArrayList<DayOfWeek> tempDays = new ArrayList<>();
-        for (DayOfWeek d : DayOfWeek.values()) {
-            System.out.println("Would you like to take on " + d + "?");
-            System.out.println("1. Yes");
-            System.out.println("2. No");
-            while (flag){
-                switch (input.nextInt()){
-                    case 1:
-                        tempDays.add(d);
-                        flag = false;
-                        break;
-                    case 2:
-                        flag = false;
-                        break;
-                    default:
-                        System.out.println("Please select a valid option");
+        boolean flag1 = true;
+        while(flag1) {
+            for (DayOfWeek d : DayOfWeek.values()) {
+                System.out.println("Would you like to take on " + d + "?");
+                System.out.println("1. Yes");
+                System.out.println("2. No");
+                while (flag) {
+                    switch (input.nextInt()) {
+                        case 1:
+                            tempDays.add(d);
+                            flag = false;
+                            break;
+                        case 2:
+                            flag = false;
+                            break;
+                        default:
+                            System.out.println("Please select a valid option");
+                    }
                 }
+                flag = true;
             }
-            flag = true;
+            if(tempDays.isEmpty()) {
+                System.out.println("Please select at least one day.");
+            }
+            else {
+                newMed.setDaysOfWeek(tempDays);
+                flag1 = false;
+            }
         }
-        newMed.setDaysOfWeek(tempDays);
-        flag = true;
         //input new medication time
         System.out.println("Please enter a time in 24 hour format (00:00): ");
         LocalTime tempTime;
         while(flag) {
             try{
+                //Fixed extra error message caused by the time set code block
+                input.nextLine();
                 tempTime = LocalTime.parse(input.nextLine());
                 newMed.setTime(tempTime);
                 flag = false;
